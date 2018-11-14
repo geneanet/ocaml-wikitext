97cd831064f81cd5395c2116d31e239c3a09a1a5
Maxence Abela <maxence.abela@geneanet.org>
2018-11-14 15:47:48 +0100

    added new types for the AST

--- wktxt_type.ml
+++ wktxt_type.ml
@@ -3,12 +3,16 @@ type document = block list
 
 and block =
   | Header of int * inline list
+  | Paragraph of inline list
+  | List of int * inline list
+  | Num_list of int * inline list
+  | Hrule
+  | Emptyline
 [@@deriving show]
 
 and inline =
   | Bold of inline list
   | Italic of inline list
-  | White
   | String of string
   | Char of char
 [@@deriving show]
