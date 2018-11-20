89f19b23f74667b72d0efa523621d24341c37b29
Maxence Abela <maxence.abela@geneanet.org>
2018-11-20 14:32:24 +0100

    fix future types for lists and added type for tables

--- src/wktxt_type.ml
+++ src/wktxt_type.ml
@@ -6,23 +6,28 @@ and block =
   | Paragraph of inline list
   | List of int * inline list
   | NumList of int * inline list
-  | Table of inline list
+  (*
+    for future list parsing
+    List of inline list
+    NumList of inline list
+  *)
+  | ListItem of block list
+  | NumListItem of block list
+  | Table of table_block list list
   | Hrule
 [@@deriving show { with_path = false }]
 
-(* à terme, List et Numlist comprendront des List_elem et NumList_elem
-  ces derniers porteront les numéros d'importance à la place de List et NumList
-*)
 and inline =
   | NamedLink of inline list
   | Link of inline list
-  | TableLine of inline list
-  | List_elem of int * inline list
-  | NumList_elem of int * inline list
   | Bold of inline list
   | Italic of inline list
-  | Bolditalic of inline list
   | String of string
+[@@deriving show { with_path = false }]
+
+and table_block =
+  | TableHead of inline list
+  | TableItem of inline list
 [@@deriving show { with_path = false }]
 
 (* [@@deriving show] va créer automatique les fonctions
