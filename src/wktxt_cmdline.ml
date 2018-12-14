d3b04ee25f352da9be19a0a4fc5ef765d94b0a2b
Maxence Abela <maxence.abela@geneanet.org>
2018-12-14 16:28:27 +0100

    Updated cmd_line for Table of Contents generation

--- src/wktxt_cmdline.ml
+++ src/wktxt_cmdline.ml
@@ -5,27 +5,26 @@ let set_table_of_content doc =
   let block self blck =
     match blck with
     | Header (depth, inlines) -> Printf.printf "Header found : depth = %d\n" depth ;
-      toc_list := ((Ordered, depth), inlines) :: !toc_list ;
+      toc_list := ((Unordered, depth), [inlines]) :: !toc_list ;
       blck
     | _ -> default_mapper.block self blck in
   let mapper = { default_mapper with block } in
-  match mapper.document mapper doc with _ -> () in
-  let toc_ast = Wktxt_parser.parse_list (List.ref !toc_list) in
+  let () = match mapper.document mapper doc with _ -> () in
+  let toc_ast = List.hd (List.flatten (Wktxt_parsing_functions.parse_list 0 (List.rev !toc_list) Unordered)) in
   let block self blck =
     match blck with
     (* TODO handle multi-string paragraphs *)
-    | Paragraph l when l = [String "__TOC__"] -> toc_ast
-    | List | NumList | DefList | _ -> blck in
+    | Paragraph l when l = [String "__TOC__" ; String "\n"] -> toc_ast
+    | List _ | NumList _ | DefList _ -> blck
+    | _ -> default_mapper.block self blck in
   let mapper = { default_mapper with block } in
   mapper.document mapper doc
 
 let () =
   let lexbuf = Lexing.from_channel stdin in
   try
-    let doc = Wktxt_parser.document Wktxt_lexer.main lexbuf in
-    print_endline (Wktxt_type.show_document doc) ;
-    match set_table_of_content doc with
-    | _ -> ()
+    let doc = (Wktxt_parser.document Wktxt_lexer.main lexbuf) in
+    print_endline (Wktxt_type.show_document doc)
   with
   | _ ->
     let curr = lexbuf.Lexing.lex_curr_p in
