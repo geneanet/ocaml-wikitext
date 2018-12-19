afe550e50957dd6940bb7d6129e655b153bbc8c3
Maxence Abela <maxence.abela@geneanet.org>
2018-12-19 14:18:25 +0100

    Added Wikitext module that contains functions.

---
+++ src/wikitext.ml
@@ -0,0 +1,32 @@
+module Mapper = Wktxt_mapper
+
+let get_current_parser_position lexbuf =
+  let curr = lexbuf.Lexing.lex_curr_p in
+  let line = curr.Lexing.pos_lnum in
+  let col = curr.Lexing.pos_cnum - curr.Lexing.pos_bol in
+  let tok = Lexing.lexeme lexbuf in
+  Printf.sprintf "line %d, col %d, token [%s]" line col tok
+
+let doc_from_lexbuf lexbuf =
+  Wktxt_lexer.newline := true ;
+  Wktxt_lexer.last_def_term_line := 0 ;
+  Wktxt_lexer.last_def_term_depth := 0 ;
+  Wktxt_lexer.in_table := false ;
+  Wktxt_parser.document Wktxt_lexer.main lexbuf
+
+let output_document =
+  Wktxt_output.output_document
+
+let doc_to_string doc =
+  let buffer = Buffer.create 4096 in
+  let () = output_document (Buffer.add_string buffer) doc in
+  Buffer.contents buffer
+
+let doc_to_chan doc chan =
+  let str_doc = doc_to_string doc in
+  output_string chan str_doc
+
+let doc_to_file doc filename =
+  let chan = open_out filename in
+  let () = doc_to_chan doc chan in
+  flush chan
