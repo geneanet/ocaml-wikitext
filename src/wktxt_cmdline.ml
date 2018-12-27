1ef004525baaa4ccc4141d2c55502222321452bd
Maxence Abela <maxence.abela@geneanet.org>
2018-12-27 11:15:23 +0100

    Removed try/catch from cmdline since it is already handled in doc_from_lexbuf.

--- src/wktxt_cmdline.ml
+++ src/wktxt_cmdline.ml
@@ -1,10 +1,5 @@
 open Wikitext
 let () =
   let lexbuf = Lexing.from_channel stdin in
-  try
-    let doc = doc_from_lexbuf lexbuf |> Mapper.set_table_of_content |> Mapper.set_links in
-    output_document (Printf.printf "%s") doc
-  with
-  | _ ->
-    failwith
-      ("Error : " ^ get_current_parser_position lexbuf)
+  let doc = doc_from_lexbuf lexbuf |> Mapper.set_table_of_content |> Mapper.set_links in
+  output_document (Printf.printf "%s") doc
