6a10b6b1e1019777c51fb1d680c21e6345da3ff1
Julien Sagot <julien.sagot@geneanet.org>
2018-12-27 07:19:11 +0100

    doc_from_lexbuf: raise an error in case of failure.

--- src/wikitext.ml
+++ src/wikitext.ml
@@ -1,18 +1,24 @@
+module Type = Wktxt_type
 module Mapper = Wktxt_mapper
 
-let get_current_parser_position lexbuf =
-  let curr = lexbuf.Lexing.lex_curr_p in
-  let line = curr.Lexing.pos_lnum in
-  let col = curr.Lexing.pos_cnum - curr.Lexing.pos_bol in
-  let lexeme = Lexing.lexeme lexbuf in
-  Printf.sprintf "line %d, col %d, lexeme [%s]" line col lexeme
+(** [ParsingError (line, column, lexeme)]  *)
+exception ParsingError of int * int * string
 
+(** [doc_from_lexbuf lex] parse [lex] and return the resulting {!type:Type.doc}.
+    Raise {!exception:ParsingError} in case of failure *)
 let doc_from_lexbuf lexbuf =
-  Wktxt_lexer.newline := true ;
-  Wktxt_lexer.last_def_term_line := 0 ;
-  Wktxt_lexer.last_def_term_depth := 0 ;
-  Wktxt_lexer.in_table := false ;
-  Wktxt_parser.document Wktxt_lexer.main lexbuf
+  try
+    Wktxt_lexer.newline := true ;
+    Wktxt_lexer.last_def_term_line := 0 ;
+    Wktxt_lexer.last_def_term_depth := 0 ;
+    Wktxt_lexer.in_table := false ;
+    Wktxt_parser.document Wktxt_lexer.main lexbuf
+  with _ ->
+    let curr = lexbuf.Lexing.lex_curr_p in
+    let line = curr.Lexing.pos_lnum in
+    let col = curr.Lexing.pos_cnum - curr.Lexing.pos_bol in
+    let lexeme = Lexing.lexeme lexbuf in
+    raise (ParsingError (line, col, lexeme))
 
 let output_document =
   Wktxt_output.output_document
