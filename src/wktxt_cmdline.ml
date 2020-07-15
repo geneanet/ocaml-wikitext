db91b81cc76bd05fb8ee56b09c1297f3f0d5a167
Julien Sagot <julien.sagot@geneanet.org>
2020-07-15 11:16:57 +0200

    Fixed compilation (toc instead of table_of_content)

--- src/wktxt_cmdline.ml
+++ src/wktxt_cmdline.ml
@@ -1,5 +1,5 @@
 open Wikitext
 let () =
   let lexbuf = Lexing.from_channel stdin in
-  let doc = doc_from_lexbuf lexbuf |> Mapper.set_table_of_content |> Mapper.set_links in
+  let doc = doc_from_lexbuf lexbuf |> Mapper.set_toc |> Mapper.set_links in
   output_document (Printf.printf "%s") doc
